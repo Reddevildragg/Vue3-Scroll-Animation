a2b4d85a382694a7a1bec0ee44ccb787c7a86765
Tom <thomas.greener@hotmail.co.uk>
2024-10-17 11:44:21 +0100

    fix formatting

--- src/App.vue
+++ src/App.vue
@@ -5,47 +5,47 @@
         This element will fade in and out when 25% visible due to scrolling.
       </div>
       <div style="height: 100vh"></div>
-<!--      &lt;!&ndash; Slide In from Left and Slide Out to Left &ndash;&gt;
+      <!-- Slide In from Left and Slide Out to Left -->
       <div>
         <div  v-animate-in-view="{ enterClass: 'slide-in-left', leaveClass: 'slide-out-left' }">
           This element will slide in from the left and slide out to the left when 25% visible due to scrolling.
         </div>
       </div>
 
-      &lt;!&ndash; Slide In from Right and Slide Out to Right &ndash;&gt;
+      <!-- Slide In from Right and Slide Out to Right -->
       <div v-animate-in-view="{ enterClass: 'slide-in-right', leaveClass: 'slide-out-right' }">
         This element will slide in from the right and slide out to the right when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Slide In from Up and Slide Out to Up &ndash;&gt;
+      <!-- Slide In from Up and Slide Out to Up -->
       <div v-animate-in-view="{ enterClass: 'slide-in-up', leaveClass: 'slide-out-up'}">
         This element will slide in from the bottom and slide out to the top when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Slide In from Down and Slide Out to Down &ndash;&gt;
+      <!-- Slide In from Down and Slide Out to Down -->
       <div v-animate-in-view="{ enterClass: 'slide-in-down', leaveClass: 'slide-out-down' }">
         This element will slide in from the top and slide out to the bottom when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Scale In and Out &ndash;&gt;
+      <!-- Scale In and Out -->
       <div v-animate-in-view="{ enterClass: 'scale-in', leaveClass: 'scale-out' }">
         This element will scale in and out when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Rotate In and Out &ndash;&gt;
+      <!-- Rotate In and Out -->
       <div v-animate-in-view="{ enterClass: 'rotate-in', leaveClass: 'rotate-out' }">
         This element will rotate in and out when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Flip In X and Flip Out X &ndash;&gt;
+      <!-- Flip In X and Flip Out X -->
       <div v-animate-in-view="{ enterClass: 'flip-in-x', leaveClass: 'flip-out-x' }">
         This element will flip in horizontally and flip out when 25% visible due to scrolling.
       </div>
 
-      &lt;!&ndash; Flip In Y and Flip Out Y &ndash;&gt;
+      <!-- Flip In Y and Flip Out Y -->
       <div v-animate-in-view="{ enterClass: 'flip-in-y', leaveClass: 'flip-out-y' }">
         This element will flip in vertically and flip out when 25% visible due to scrolling.
-      </div>-->
+      </div>
     </div>
 </template>
 
